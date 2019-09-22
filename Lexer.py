8d0aee561bc8c80f02afe4d7263fd8e38d4b221a
Edward Ramirez <blueeaidle360@yahoo.com>
2019-09-21 20:26:21 -0700

    Integrated Angel's code with modifications to correctly split words into tokens

--- Lexer.py
+++ Lexer.py
@@ -1,3 +1,49 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("\nPlease input file name as argument. Usage example: \"python Lexer.py SampleInputFile.txt\" \n")
+    raise Exception
+
+def process_file(file_name):
+    #all lines into a string from text file
+    with open(file_name) as f:
+        text = f.read()
+
+    #single-char words
+    separators = ['\'', '(', ')', '{', '}', '[', ']', ',', '.', ':', ';']
+
+    # single-line comments
+    single_line_comments = ['#', '//', '!']
+
+    # Break into lines
+    text = text.split('\n')
+
+    # Remove single-line comments
+    no_comment_text = []
+    for line in text:
+        valid = True
+        for token in single_line_comments:
+            if line.startswith(token):
+                valid = False
+        if valid:
+            no_comment_text.append(line)
+    text = ' '.join(no_comment_text)
+
+    # Remove multi-lines
+    text = " ".join(text.split())
+
+    # Split separators from other words
+    new_text = ''
+    for char in text:
+        if char in separators:
+            new_text += ' ' + char + ' '
+        else:
+            new_text += char
+
+    text = new_text.split()
+
+    return text
+
 
 def identifier_fsm(token):
     """
@@ -90,31 +136,32 @@ def int_fsm(token):
 def lexer(token):           #defines a function
     separators = "'(){}[],.:;!"
     if token in separators or token == ' ':
-        return "SEPARATOR"
+        return "SEPARATOR   =   "
 
     operators = ["*", "+", "-", "=", "/", "<", ">", "<=", ">=", "%"]
     if token in operators:
-        return "OPERATOR"
+        return "OPERATOR    =   "
 
     keywords = ["int", "float", "bool", "if", "else", "then", "endif", "while", "whileend", "do", "doend", "for", "forend", "input", "output", "and", "or", "function"]
     if token in keywords:
-        return "KEYWORD"
+        return "KEYWORD     =   "
     
     is_identifier = identifier_fsm(token)
 
     if is_identifier:
-        return "IDENTIFIER"
+        return "IDENTIFIER  =   "
 
     is_real = reals_fsm(token)
 
     if is_real:
-        return "REAL     "
+        return "REAL        =   "
 
     is_int = int_fsm(token)
 
     if is_int:
-        return "INTEGER "
+        return "INTEGER     =   "
 
-test = "while ( fahr < upper ) a = 23.00 whileend while ( cr < lower ) b = 14 whileend"
-for word in test.split():
-    print (lexer(word), "\t", word)
+text = process_file(sys.argv[1])
+print("TOKEN            Lexemes\n")
+for word in text:
+    print (lexer(word), word)
