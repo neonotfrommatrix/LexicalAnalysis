4b7695ef305430db7f2b1e9d6b58d16c892eead8
neonotfrommatrix <ottolianatalie@gmail.com>
2019-12-13 23:01:50 -0800

    Partially working

--- Compiler.py
+++ Compiler.py
@@ -1,7 +1,9 @@
 from Lexer import lexer
 
+BASE_MEMLOC = 2000
 memory_table = []
 instructions = []
+STACK = []
 DATATYPES = ['int', 'float', 'bool', 'boolean']
 
 # New declarations
@@ -38,6 +40,46 @@ def is_assignment(statement):
     if lexer(tokens[2]) not in ['IDENTIFIER', 'INTEGER', 'BOOLEAN']:
         return False
     return True
+
+def table_add(statement):
+    tokens = statement.split()
+    tuple_type = tokens[0]
+    for id in [token for token in tokens if lexer(token) == 'IDENTIFIER']:
+        tup = (id, BASE_MEMLOC + len(memory_table), tuple_type)
+        memory_table.append(tup)
+
+    # print(memory_table)
+
+def instruction_assignment(statement):
+    tokens = statement.split()
+    val = tokens[2]
+    id = tokens[0]
+    if lexer(val) == 'BOOLEAN':
+        val = 0 if val == 'false' else 1
+    ins_type = 'PUSHI'
+    instructions.append('{}    {}    {}'.format(len(instructions) + 1, ins_type, val))
+    for tup in memory_table:
+        if tup[0] == id:
+            val = tup[1]
+    instructions.append('{}    {}    {}'.format(len(instructions) + 1, 'POPM', val))
+
+sample = ['int num , nu2m , large$ ;',
+        'num = 0 ;',
+        'nu2 = 15 ;',
+        'boolean hey ;',
+        'hey = true ;']
+
+table_add(sample[0])
+instruction_assignment(sample[1])
+instruction_assignment(sample[2])
+instruction_assignment(sample[3])
+instruction_assignment(sample[4])
+
+print(*instructions, sep='\n')
+print('Symbol Table')
+print('Identifier   MemoryLocation  Type')
+for tup in memory_table:
+    print('{}        {}               {}'.format(tup[0], tup[1], tup[2]))
 # If
 
 # While
